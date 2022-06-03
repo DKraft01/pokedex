4a52606e4b4ed3537e3e3bd598c5672fce1abbe1
DKraft <danielkraft2022@gmail.com>
2022-06-03 15:19:00 -0500

    not finished 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PokeFinder></PokeFinder>
     </div>
   );
+}
+
+class PokeFinder extends React.Component {
+  state = {
+    dato: [],
+
+    max: 151,
+  };
+
+  async componentDidMount() {
+    var hola = [];
+    for (let i = 1; i <= this.state.max; i++) {
+      const dato = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const datoF = await dato.json();
+      console.log(datoF);
+      let name = datoF.name;
+      hola.push(name);
+    }
+    this.setState({ dato: hola });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.max !== this.state.max) {
+      this.componentDidMount();
+    }
+  }
+
+  render() {
+    let arr2 = [];
+
+    for (var i = 1; i <= this.state.max; i++) {
+      arr2.push(i);
+    }
+
+    const numberOfPokemons = (e) => {
+      e ? this.setState({ max: e }) : this.setState({ max: 151 });
+    };
+
+    return (
+      <>
+        <div className="container">
+          <h1>Pokedex</h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              placeholder="Number of Pokemons"
+              type="number"
+              onChange={(e) => numberOfPokemons(e.target.value)}
+            ></input>
+            <button>Go!</button>
+          </form>
+          {arr2.map((e) => {
+            let f;
+            if (e < 10) {
+              f = "00" + e;
+            } else if (e >= 10 && e < 100) {
+              f = "0" + e;
+            } else {
+              f = e;
+            }
+
+            let foto = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${f}.png`;
+            return (
+              <div className="pokeball" key={e}>
+                <h5>#{f}</h5>
+                <img
+                  onClick={() =>
+                    window.open(
+                      `https://www.wikidex.net/wiki/${this.state.dato[e - 1]}`
+                    )
+                  }
+                  className="poke"
+                  src={foto}
+                  alt={this.state.dato[e - 1]}
+                />
+                <p>{this.state.dato[e - 1]}</p>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
